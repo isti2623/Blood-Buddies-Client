40c54d6ef4ba133bb560f1b6a1cf5f397481994f
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-11-06 17:48:07 +0600

    responsive bug fixed

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -14,8 +14,7 @@ const Banner = () => {
                         <div className='text-part'>
                             <h4 className='text-danger fs-2 ms-1'>Donate blood , save life!</h4>
                             <h1 className='text-white ms-1 custom-h1'>Donate Blood And Inspires Others.</h1>
-                            <p className='text-white ms-1'> We are working all over the world, organizing blood donation campaign to grow awareness among the people to donate blood.</p>
-                            <Link to='needDoners'> <Button className='mt-3 rounded p-3' variant="danger fw-bold">DONATE NOW</Button></Link>
+                            <Link to='needDoners'> <Button className='mt-2 ms-2 rounded p-3' variant="danger fw-bold">DONATE NOW</Button></Link>
                         </div>
                     </div>
                     <div className="col-lg-6">

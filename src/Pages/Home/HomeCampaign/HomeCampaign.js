46bed43fb6207d5657a67b0bbc1ac048e44a1c62
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-11-07 00:50:47 +0600

    become a volunteer button

--- src/Pages/Home/HomeCampaign/HomeCampaign.js
+++ src/Pages/Home/HomeCampaign/HomeCampaign.js
@@ -15,7 +15,7 @@ const HomeCampaign = () => {
                             <span className='divider div-head ms-3'></span>
                             <p className='mt-3 ms-3'>All over the world we have arranged total sixty thousands donation campaign and visit thousands of other venues on various occasions.
                             </p>
-                            <Link to='/campaign'> <div className="btn btn-danger p-3 ms-2 my-3">LOAD ALL CAMPAIGNS</div></Link>
+                            <Link to='/campaign'> <div className="btn btn-danger p-3 ms-2 my-3 fw-bold">LOAD ALL CAMPAIGNS</div></Link>
                         </div>
                     </div>
                     <div className="col-lg-6 my-5">
--- src/Pages/Home/Volunteers/Volunteers.js
+++ src/Pages/Home/Volunteers/Volunteers.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import teamOne from '../../../images/team_1.jpg'
 import teamTwo from '../../../images/team_2.jpg'
 import teamThree from '../../../images/team_3.jpg'
@@ -12,10 +13,10 @@ const Volunteers = () => {
                     <div className="col-lg-5"></div>
                     <div className="col-lg-7 text-center my-5 p-3 text-card shadow">
                         <div>
-                            <p className='mt-3 ms-3 fs-1 text-white'>OUR VOLUNTEERS
+                            <p className=' ms-3 fs-1 text-white'>OUR VOLUNTEERS
                             </p>
                             <span className='divider div-head ms-5'></span>
-                            <h1 className='fw-bold ms-3 fs-1 mt-2 text-white'>The volunteers who give their time and talents help to fulfill our mission.
+                            <h1 className='fw-bold ms-3 fs-1 text-white mb-2'>The volunteers who give their time and talents help to fulfill our mission.
                             </h1>
                         </div>
                     </div>
@@ -56,9 +57,19 @@ const Volunteers = () => {
                                 </Card.Body>
                             </Card>
                         </div>
+
+                    </div>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-4"></div>
+                            <div className="col-lg-8">
+                                <Link to='/campaign'> <div className="btn btn-danger p-3 ms-4 mt-5 fw-bold">BECOME A VOLUNTEER</div></Link>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
+
             </div>
 
 

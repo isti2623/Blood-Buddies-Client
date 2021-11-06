408cf6ba1d9de98df2817b616f59ba4b6d179d2d
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-11-06 17:45:19 +0600

    added banner

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+
+        <div className=' banner mt-2'>
+            <div className="container">
+                <div className="row">
+
+                    <div className="col-lg-6">
+                        <div className='text-part'>
+                            <h4 className='text-danger fs-2 ms-1'>Donate blood , save life!</h4>
+                            <h1 className='text-white ms-1 custom-h1'>Donate Blood And Inspires Others.</h1>
+                            <p className='text-white ms-1'> We are working all over the world, organizing blood donation campaign to grow awareness among the people to donate blood.</p>
+                            <Link to='needDoners'> <Button className='mt-3 rounded p-3' variant="danger fw-bold">DONATE NOW</Button></Link>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from '../../Shared/Navigation/Navigation';
 import TopHeader from '../../Shared/TopHeader/TopHeader';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
             <TopHeader></TopHeader>
             <Navigation></Navigation>
+            <Banner></Banner>
         </div>
     );
 };

52e92698ded42c799dd9f9850f8b1b0d48c6818f
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-11-12 02:10:46 +0600

    upd

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 import Navigation from '../Shared/Navigation/Navigation';
+import login from '../../images/login.jpg'
 
 const Login = () => {
     return (
         <div>
             <Navigation></Navigation>
-            <Form className='w-50 my-5 ms-5'>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <Form className='w-50 my-5 ms-5'>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button variant="danger" type="submit">
+                                Login
+                            </Button>
+                        </Form>
+                    </div>
+                    <div className="col-lg-6">
+                        <img src={login} alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };

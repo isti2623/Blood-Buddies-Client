e48e7b8aa5018aa0e164a6f9b318a2a863610fa4
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-11-19 00:08:34 +0600

    added private route

---
+++ src/Pages/Login/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+import { Navigate, Route } from 'react-router';
+import useAuth from '../../../hooks/useAuth';
+
+const PrivateRoute = ({ children, ...rest }) => {
+    const { user, isLoading } = useAuth();
+    if (isLoading) { return <Spinner animation="border" variant="danger" /> }
+    return (
+        <Route
+            {...rest}
+            render={({ location }) =>
+                user.email ? (
+                    children
+                ) : (
+                    <Navigate to="/login" state={{ from: location }} />
+                )
+            }
+        />
+    );
+};
+
+export default PrivateRoute;

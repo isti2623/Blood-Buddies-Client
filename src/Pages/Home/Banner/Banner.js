782cd84c7f18d5fa72fd5bec2d604d32828cbb31
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-11-20 17:12:26 +0600

    update

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
 import './Banner.css';
 
 const Banner = () => {
+    const { user } = useAuth();
     return (
 
         <div className=' banner mt-2'>
@@ -12,7 +14,8 @@ const Banner = () => {
 
                     <div className="col-lg-6">
                         <div className='text-part'>
-                            <h4 className='text-danger fs-2 ms-1'>Donate blood , save life!</h4>
+                            <h4 className='text-danger fs-2 ms-1'><span>Hello <span className="text-white fw-bold">{user.displayName}</span>,</span> <br />
+                                Donate blood , save life!</h4>
                             <h1 className='text-white ms-1 custom-h1'>Donate Blood And Inspires Others.</h1>
                             <Link to='needDoners'><Button className='mt-2 ms-2 rounded p-3' variant="danger fw-bold">DONATE NOW</Button></Link>
                         </div>

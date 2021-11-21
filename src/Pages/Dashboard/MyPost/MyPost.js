d7ed30f675ef37c5ef4b97ed4899eb11de643971
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-11-21 21:21:55 +0600

    update

--- src/Pages/Dashboard/MyPost/MyPost.js
+++ src/Pages/Dashboard/MyPost/MyPost.js
@@ -6,12 +6,13 @@ import SinPost from '../SinPost/SinPost';
 const MyPost = () => {
     const { user } = useAuth();
     const [posts, setPosts] = useState([]);
+    const [del, setDel] = useState(0);
     useEffect(() => {
         const url = `http://localhost:5000/bloodPostReqDashboard?email=${user.email}`
         fetch(url)
             .then(res => res.json())
             .then(data => setPosts(data))
-    }, [])
+    }, [del])
     return (
         <div>
             <h2 className='text-center fw-bold'>{user.displayName} Posted : {posts.length}</h2>
@@ -20,6 +21,8 @@ const MyPost = () => {
                 posts.map(post => <SinPost
                     key={post._id}
                     post={post}
+                    del={del}
+                    setDel={setDel}
                 >
                 </SinPost>)
             }
